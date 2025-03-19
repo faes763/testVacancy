e7d66b684644a9dff633258ff4107368c1ccfbc8
wr479 <normar00@mail.ru>
2025-03-19 15:34:05 +0400

    add

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex  items-center justify-items-center min-h-screen bg-white">
+    <div className="flex  items-center justify-center min-h-screen bg-white">
       <div className="flex flex-col  bg-black h-8 w-8">
       
       </div>
